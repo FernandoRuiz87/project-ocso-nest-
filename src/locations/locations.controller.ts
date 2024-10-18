78969dec2726c8aeaad8a616fd9fe73124c61a61
FernandoRuiz87 <164447372+FernandoRuiz87@users.noreply.github.com>
2024-10-18 15:08:32 -0600

    fix: Return location

--- src/locations/locations.controller.ts
+++ src/locations/locations.controller.ts
@@ -15,7 +15,7 @@ import { ROLES } from "src/auth/constants/roles.constants";
 import { ApiAuth } from "src/auth/decorators/api.decorator";
 import { ApiTags } from "@nestjs/swagger";
 
-// @ApiAuth()
+@ApiAuth()
 @ApiTags("Locations")
 @Controller("locations")
 export class LocationsController {
@@ -27,7 +27,7 @@ export class LocationsController {
     return this.locationsService.create(createLocationDto);
   }
 
-  // @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get()
   findAll() {
     return this.locationsService.findAll();
